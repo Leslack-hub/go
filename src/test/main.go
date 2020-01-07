4b64ffe9a44c9abb8b13a80a15d07e1524caef60
lishanchuan <lishanchuan@mocaapp.cn>
2020-01-07 21:01:39 +0800

    test code commit

--- src/test/main.go
+++ src/test/main.go
@@ -1,22 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
 
 const text = `my name is haha@lishuaishuai.com 
 fddsafds@fdsaf.com.cn
 `
 
 func main() {
-	strings := []string{"hello.com"}
-	fmt.Println(strings)
-	//str := []int{1,2,3,4}
-	//for _, i := range str {
-	//	fmt.Println(i)
-	//}
-
-	//re := regexp.MustCompile(`([a-zA-Z0-9]+)@([a-zA-Z0-9]+)(\.[a-zA-Z0-9.]+)`)
-	//match := re.FindAllStringSubmatch(text, -1)
-	//for _, m := range match {
-	//	fmt.Println(m)
-	//}
+	// 正则 测试
+	re := regexp.MustCompile(`([a-zA-Z0-9]+)@([a-zA-Z0-9]+)(\.[a-zA-Z0-9.]+)`)
+	match := re.FindAllStringSubmatch(text, -1)
+	for _, m := range match {
+		fmt.Println(m)
+	}
 }
