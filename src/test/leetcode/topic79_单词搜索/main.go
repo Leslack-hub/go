4d5aa5362e71da19141a154e40155bf24a9afe15
lishanchuan <lishanchuan@mocaapp.cn>
2020-06-15 20:59:59 +0800

    daliy commit

--- src/test/leetcode/topic79_单词搜索/main.go
+++ src/test/leetcode/topic79_单词搜索/main.go
@@ -1,20 +1,61 @@
 package main
 
-var directions = [][]int{
-	{0, -1}, // 左
-	{0, 1},  // 右
-	{1, 0},  // 下
-	{-1, 0}, // 上
-}
+import "fmt"
+
+var b [][]byte
+var w string
 
 func exist(board [][]byte, word string) bool {
-
+	m, n := len(board), len(board[0])
+	b = board
+	w = word
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if dfs(i, j, 0) {
+				return true
+			}
+		}
+	}
+	return false
 }
 
+func dfs(i, j, k int) bool {
+	if k == len(w) {
+		return true
+	}
+
+	if i < 0 ||
+		j < 0 ||
+		i == len(b) ||
+		j == len(b[i]) {
+		return false
+	}
+	if b[i][j] != w[k] {
+		return false
+	}
+	fmt.Println(i, j, k)
+	tmp := b[i][j]
+	b[i][j] = byte(' ')
+
+	if dfs(i-1, j, k+1) {
+		return true
+	}
+	if dfs(i+1, j, k+1) {
+		return true
+	}
+	if dfs(i, j-1, k+1) {
+		return true
+	}
+	if dfs(i, j+1, k+1) {
+		return true
+	}
+	b[i][j] = tmp
+	return false
+}
 func main() {
-	exist([][]byte{
+	fmt.Println(exist([][]byte{
 		{'A', 'B', 'C', 'E'},
 		{'S', 'F', 'C', 'S'},
 		{'A', 'D', 'E', 'E'},
-	}, "SEE")
+	}, "ACE"))
 }
