ea640e2d9cbb5d90a8c8a7bae805a10c5cd54795
lishanchuan <lishanchuan@mocaapp.cn>
2020-07-17 20:51:24 +0800

    daily commit

--- src/test/lintcoode/topic76/main.go
+++ src/test/lintcoode/topic76/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+/**
+ * 动态规划
+ */
 func LongestIncreasingSubsequence(A []int) int {
 	n := len(A)
 	f := make([]int, n)
@@ -24,6 +28,36 @@ func LongestIncreasingSubsequence(A []int) int {
 	return max
 }
 
+/**
+ *优化方法
+ */
+func LongestIncreasingSubsequence2(A []int) int {
+	length := len(A)
+	b := make([]int, length+1)
+	start, end := 0, 0
+	b[0] = math.MinInt32
+	top := 0
+	for i := 0; i < length; i++ {
+		j := 0
+		end = top
+		// 二分法
+		for start <= end {
+			mid := (start + end) / 2
+			if b[mid] < A[i] {
+				j = mid
+				start = mid + 1
+			} else {
+				end = mid - 1
+			}
+		}
+		b[j+1] = A[i]
+		if j+1 > top {
+			top = j + 1
+		}
+	}
+	return top
+}
+
 func main() {
-	fmt.Println(LongestIncreasingSubsequence([]int{4, 2, 4, 5, 3, 7}))
+	fmt.Println(LongestIncreasingSubsequence2([]int{4, 2, 4, 5, 3, 7}))
 }
